e9a37b882e32a3ab709f8570840728df27b4afc9
Dmitry <dymonius12@gmail.com>
2017-09-21 13:23:11 +0300

    minimize was added

--- script.js
+++ script.js
@@ -1,31 +1,17 @@
 var stickElement = document.querySelector(".menu");
-var stuck = false;
-var stickHeight = getHeight();
-var stickPoint = getDistance();
+var fixed = false;
+var stickPoint = stickElement.offsetTop;
 
-
-
-function getHeight() {
-    var height = stickElement.offsetHeight;
-    return height;
-
-}
-
-function getDistance() {
-    var topDist = stickElement.offsetTop;
-    return topDist;
-}
-
-window.onscroll = function (e) {
-    var distance = getDistance() - window.pageYOffset;
-    var offset = window.pageYOffset;
-    if ((distance <= 0) && !stuck) {
+window.onscroll = function () {
+    var distance = stickElement.offsetTop - window.pageYOffset;
+    var offset = window.pageYOffset; //Получение текущей прокрутки сверху
+    if ((distance <= 0) && !fixed) {
         stickElement.style.position = 'fixed';
         stickElement.style.top = '0px';
 
-        stuck = true;
-    } else if (stuck && (offset <= stickPoint)) {
+        fixed = true;
+    } else if (fixed && (offset <= stickPoint)) {
         stickElement.style.position = 'static';
-        stuck = false;
+        fixed = false;
     }
-}
+};
