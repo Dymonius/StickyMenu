0ce4c88d94f45a7f228e8385c23336efd6b0def9
Dmitry <dymonius12@gmail.com>
2017-10-11 15:03:52 +0300

    OOP version was added

--- script.js
+++ script.js
@@ -16,3 +16,31 @@ window.onscroll = function () {
         fixed = false;
     }
 };
+// OOP style
+
+
+// function Sticky(obj) {
+//     var sticky = this;
+//
+//     sticky.stickElement = document.querySelector(obj.stickElement);
+//     var fixed = false;
+//     var fixPoint = sticky.stickElement.offsetTop; //Получение расстояния по оси Y от верха страницы до элемента
+//
+//     window.onscroll = function () {
+//         var distance = fixPoint - window.pageYOffset;
+//         var offset = window.pageYOffset; //Получение текущей прокрутки сверху
+//         if ((distance <= 0) && !fixed) {
+//             sticky.stickElement.classList.add('menu--fixed');
+//
+//             fixed = true;
+//         } else if (fixed && (offset <= fixPoint)) {
+//             sticky.stickElement.classList.remove('menu--fixed');
+//
+//             fixed = false;
+//         }
+//     };
+// }
+//
+// new Sticky({
+//     stickElement: '.menu'
+// });
